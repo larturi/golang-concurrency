cf725b1a22559d4af56e841fcf3c21f68dc87257
Leandro Arturi <lea.arturi@gmail.com>
2023-03-25 10:05:58 -0300

    Sync Cuenta Bancaria con Lock()

--- main.go
+++ main.go
@@ -5,7 +5,23 @@ import (
 )
 
 func main() {
+
+	/*
+	* Consume una API de manera concurrente
+	 */
 	// labs.StartTodos()
 
-	labs.StartHexa()
+	/*
+	* Genera un txt con la lista de numeros hexadecimales con concurrencia
+	 */
+	// labs.StartHexa()
+
+	/*
+	* Sincronización de una cuenta bancaria con depositos y extractos
+	* Warning de DATA RACE
+	* > go build --race main.go
+	* > ./main
+	* Se resuelve usando Lock() y Unlock()
+	 */
+	labs.StartSyncCuentaBancaria()
 }
